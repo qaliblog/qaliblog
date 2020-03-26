da37e1b3ddf2381fd72336b25b933152eff33722
QaliBlog <qaliblog9876@gmail.com>
2020-03-26 12:07:08 -0400

    Add association between articles and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  before_save {self.email = email.downcase}
   validates :username, presence: true, uniqueness: {case_sensitive: false},
-            length: (minimum: 3, maximum: 25)
+            length: {minimum: 3, maximum: 25}
 
-  VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i          
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
             uniqueness: {case_sensitive: false},
-            length: (maximum: 105), format: {with: VALID_EMAIL_REGEX}
+            length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
+end
