9b468397038ed8ae625cf0c7ab284da186bed5c5
QaliBlog <qaliblog9876@gmail.com>
2020-03-28 15:10:58 -0400

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :destroy, :edit, :update]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -24,7 +26,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfuly created"
       redirect_to article_path(@article)
@@ -47,6 +49,11 @@ class ArticlesController < ApplicationController
   end
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
 
   def index
     @users = User.all
@@ -9,17 +11,15 @@ class UsersController < ApplicationController
   def create
    @user = User.new(user_params)
    if @user.save
-     flash[:success] = "welcome to alphablog #{@user.username}"
+     flash[:success] = "welcome to Qaliblog #{@user.username}"
      redirect_to articles_path
    else
      render 'new'
    end
   end
   def edit
-   @user = User.find(params[:id])
   end
   def update
-   @user = User.find(params[:id])
    if @user.update(user_params)
      flash[:success] = "Your account was updated successfully"
      redirect_to articles_path
@@ -28,17 +28,19 @@ class UsersController < ApplicationController
    end
   end
   def show
-   @user = User.find(params[:id])
   end
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
-    @user = User.new(user_params)
-    if @user.save
-    flash[:success] = "welcome to alphablog #{@user.username}"
-    redirect_to articles_path
-    else
-    render 'new'
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
     end
   end
+
 end
