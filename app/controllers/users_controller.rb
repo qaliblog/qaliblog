efef1a2d8ffceaf1752c65e85f753fe6343e3a31
QaliBlog <qaliblog9876@gmail.com>
2020-03-28 05:16:24 -0400

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,12 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-  def indexs
-=======
+
   def index
->>>>>>> Admin-users-show
     @users = User.all
   end
   def new
     @user = User.new
   end
   def create
-<<<<<<< HEAD
    @user = User.new(user_params)
    if @user.save
      flash[:success] = "welcome to alphablog #{@user.username}"
@@ -36,8 +32,7 @@ class UsersController < ApplicationController
   end
   private
   def user_params
-   params.require(:user).permit(:username, :email, :password)
-=======
+    params.require(:user).permit(:username, :email, :password)
     @user = User.new(user_params)
     if @user.save
     flash[:success] = "welcome to alphablog #{@user.username}"
@@ -45,25 +40,5 @@ class UsersController < ApplicationController
     else
     render 'new'
     end
-  end
-  def edit
-    @user = User.find(params[:id])
-  end
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-    flash[:success] = "Your account was updated successfully"
-    redirect_to articles_path
-    else
-    render 'edit'
-    end
-  end
-  def show
-    @user = User.find(params[:id])
-  end
-  private
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
->>>>>>> Admin-users-show
   end
 end
